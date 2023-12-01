7038e6ee3bdbba9252ff322c71d5131293ad7b5e
arkam-mohamad <125960377+arkam-mohamad@users.noreply.github.com>
2023-12-01 17:08:53 +0530

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,1 +1,10 @@
+let plus = $('.plus');
+let minus = $('.minus');
+
+$(plus).click(function () { 
+  $($(this).parent().parent()).toggleClass('active');
+});
+$(minus).click(function () { 
+  $($(this).parent().parent()).toggleClass('active');
+});
 
